6c9565a3650f5d992201193a75f6277a7e8c0aa0
-Unknown- <mrunknownyt@gmail.com>
2026-02-16 19:16:21 -0700

    Resources & Todo

--- Src/Test/VulkanTest.cs
+++ Src/Test/VulkanTest.cs
@@ -8,33 +8,6 @@ using OpenTK.Graphics.Vulkan;
 using OpenTK.Mathematics;
 
 namespace Engine3.Test.Test {
-	// # resources
-	// https://vulkan-tutorial.com/
-	// https://vkguide.dev/
-	// https://lesleylai.info/en/vk-khr-dynamic-rendering/
-	// https://developer.nvidia.com/vulkan-memory-management
-	// https://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/#the-view-matrix
-	// https://vulkan.lunarg.com/doc/view/1.4.304.0/linux/best_practices.html
-	// https://github.com/KhronosGroup/Vulkan-ValidationLayers/blob/main/docs/debug_printf.md
-	// https://github.com/ocornut/imgui/blob/master/docs/BACKENDS.md
-	// https://github.com/ocornut/imgui/blob/master/docs/BACKENDS.md#rendering-adding-support-for-imguibackendflags_rendererhastextures-192 add if i update ImGui
-	// https://github.com/ocornut/imgui/blob/master/docs/FAQ.md#q-how-can-i-create-my-own-backend
-	// https://docs.vulkan.org/tutorial/latest/Building_a_Simple_Engine/GUI/02_imgui_setup.html
-	// https://github.com/ocornut/imgui/blob/master/backends/imgui_impl_vulkan.cpp
-	// https://github.com/ocornut/imgui/blob/master/backends/imgui_impl_opengl3.cpp
-	// TODO make resources.md
-
-	// # where i'm at
-	// https://vulkan-tutorial.com/Loading_models
-
-	// TODO fix white screen while resizing. is it possible to show the last swap frame and scale then present?
-	// TODO figure out how to dynamically change images. do i update descriptors each time?
-	// TODO add way more debug logging. i kinda want more levels though. look into that. maybe redo logging in general. multiple files or single file?
-	// TODO read https://docs.vulkan.org/samples/latest/samples/extensions/descriptor_indexing/README.html
-	// TODO read https://docs.vulkan.org/guide/latest/buffer_device_address.html
-	// TODO setup ImPlot
-	// TODO use more stackallocs with vulkan
-
 	public class VulkanTest : GameClient {
 		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 

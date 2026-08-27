5350cb4b662f7fd822013d61874bafd374c25b9b
-Unknown- <Contact@MrUnknown.dev>
2026-08-27 15:48:47 -0600

    update

--- Src/Entry.cs
+++ Src/Entry.cs
@@ -1,13 +1,10 @@
-using Engine4.Graphics;
 using Engine4.IO;
 
 namespace Engine4.Test;
 
 public static class Entry {
-	public const GraphicsApi GraphicsApi = Engine4.Graphics.GraphicsApi.Vulkan;
-
 	public static void Main(string[] args) {
-		using (Engine engine = new(args, GraphicsApi, new OpenTKEventHandler())) {
+		using (Engine engine = new(args, new OpenTKEventHandler())) {
 			TestGame game = new(engine);
 			engine.Start(game);
 
--- Src/TestGame.cs
+++ Src/TestGame.cs
@@ -1,35 +1,39 @@
 using Engine4.Graphics;
 using Engine4.Graphics.Rendering;
 using Engine4.Graphics.Windowing;
-using OpenTK.Platform;
-using GraphicsApi = Engine4.Graphics.GraphicsApi;
 
 namespace Engine4.Test;
 
 public class TestGame : GameClient {
-	public Window Window { get => field ?? throw new NullReferenceException(); private set; }
-	public Renderer Renderer { get => field ?? throw new NullReferenceException(); private set; }
-	public TestRenderPass RenderPass { get => field ?? throw new NullReferenceException(); private set; }
+	public Window VulkanWindow { get => field ?? throw new NullReferenceException(); private set; }
+	public Renderer VulkanWindowRenderer { get => field ?? throw new NullReferenceException(); private set; }
+	public TestRenderPass VulkanWindowRenderPass { get => field ?? throw new NullReferenceException(); private set; }
 
-	public TestGame(Engine engine) : base(engine, engine.GraphicsApi switch {
-			GraphicsApi.None => null,
-			GraphicsApi.OpenGL => new OpenGLGraphicsApiHints(),
-			GraphicsApi.Vulkan => new VulkanGraphicsApiHints(),
-			GraphicsApi.Software => null,
-			_ => throw new ArgumentOutOfRangeException(),
-	}) =>
-			OnSetupDoneEvent += OnSetupDone;
+	public Window OpenGLWindow { get => field ?? throw new NullReferenceException(); private set; }
+	public Renderer OpenGLWindowRenderer { get => field ?? throw new NullReferenceException(); private set; }
+	public TestRenderPass OpenGLWindowRenderPass { get => field ?? throw new NullReferenceException(); private set; }
+
+	public TestGame(Engine engine) : base(engine, "Test Game", GraphicsApis.Vulkan | GraphicsApis.OpenGL, true) => OnSetupDoneEvent += OnSetupDone;
 
 	private void OnSetupDone() {
 		Console.WriteLine("done");
 
-		Window = CreateWindow("title goes here", 854, 480);
-		WindowRenderTarget renderTarget = new(Window); // TODO this action requires OpenTK. how do i handle that? opentk flag?
+		VulkanWindow = CreateWindow(GraphicsApi.Vulkan, "vulkan. title goes here", 854, 480); // TODO this action requires OpenTK. how do i handle that? opentk flag?
+		WindowRenderTarget vulkanRenderTarget = new(this, VulkanWindow);
 
-		RenderPass = new();
-		Renderer = CreateRenderer(renderTarget, RenderPass);
+		OpenGLWindow = CreateWindow(GraphicsApi.OpenGL, "opengl. title goes here", 854, 480); // TODO this action requires OpenTK. how do i handle that? opentk flag?
+		WindowRenderTarget openglRenderTarget = new(this, OpenGLWindow);
 
-		Window.Show();
+		VulkanWindowRenderPass = new();
+		OpenGLWindowRenderPass = new();
+
+		VulkanWindowRenderer = CreateRenderer(vulkanRenderTarget, VulkanWindowRenderPass);
+		OpenGLWindowRenderer = CreateRenderer(openglRenderTarget, OpenGLWindowRenderPass);
+
+		VulkanWindow.Show();
+		OpenGLWindow.Show();
+
+		Console.WriteLine("show");
 
 		// RequestShutdown();
 	}

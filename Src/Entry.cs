b405ee64e69202d15b84fcd8e031a1b90d95ad3d
-Unknown- <Contact@MrUnknown.dev>
2026-09-03 01:23:15 -0600

    Removed tests

--- Src/Entry.cs
+++ Src/Entry.cs
@@ -2,16 +2,8 @@ namespace Engine4.Test;
 
 public static class Entry {
 	public static void Main(string[] args) {
-		TestGame2 game = new(args);
+		TestGame game = new(args);
 		game.Start();
 		Console.WriteLine("exit");
-
-		// using (Engine engine = new(args)) {
-		// 	TestGame game = new(engine);
-		// 	engine.Start(game);
-		//
-		// 	// loop exit
-		// 	Console.WriteLine("exit");
-		// }
 	}
 }
--- Src/TestGame.cs
+++ Src/TestGame.cs
@@ -1,66 +1,49 @@
 using Engine4.Client;
-using Engine4.Client.Graphics;
 using Engine4.Client.Rendering;
 
 namespace Engine4.Test;
 
 public class TestGame : GameClient {
-	public Window VulkanWindow { get => field ?? throw new NullReferenceException(); private set; }
-	public Renderer VulkanWindowRenderer { get => field ?? throw new NullReferenceException(); private set; }
-	public TestRenderPass VulkanWindowRenderPass { get => field ?? throw new NullReferenceException(); private set; }
+	public Window? VulkanWindow0 { get; private set; }
+	public Window? VulkanWindow1 { get; private set; }
 
-	public Window OpenGLWindow { get => field ?? throw new NullReferenceException(); private set; }
-	public Renderer OpenGLWindowRenderer { get => field ?? throw new NullReferenceException(); private set; }
-	public TestRenderPass OpenGLWindowRenderPass { get => field ?? throw new NullReferenceException(); private set; }
+	public WindowRenderTarget VulkanWindow0RenderTarget { get; private set; } = null!;
+	public WindowRenderTarget VulkanWindow1RenderTarget { get; private set; } = null!;
+	public ConsoleRenderTarget ConsoleRenderTarget { get; private set; } = null!;
 
-	public Window SoftwareWindow { get => field ?? throw new NullReferenceException(); private set; }
-	public Renderer SoftwareWindowRenderer { get => field ?? throw new NullReferenceException(); private set; }
-	public TestRenderPass SoftwareWindowRenderPass { get => field ?? throw new NullReferenceException(); private set; }
+	public Renderer VulkanWindow0Renderer { get; private set; } = null!;
+	public Renderer VulkanWindow1Renderer { get; private set; } = null!;
+	public Renderer ConsoleRenderer { get; private set; } = null!; // TODO only allow 1
 
-	public Window EmptyWindow { get => field ?? throw new NullReferenceException(); private set; }
-	public Renderer EmptyWindowRenderer { get => field ?? throw new NullReferenceException(); private set; }
-	public TestRenderPass EmptyWindowRenderPass { get => field ?? throw new NullReferenceException(); private set; }
+	public RenderPass TestRenderPass { get; private set; } = null!; // TODO do i want to share this or each have their own?
 
-	public TestGame(Engine engine) : base(engine, "Test Game", GraphicsApis.Vulkan | GraphicsApis.OpenGL) => OnSetupDoneEvent += OnSetupDone;
+	public TestGame(string[] args) : base(args, "Test Game", true, true) => OnSetupDoneEvent += OnSetupDone;
 
 	private void OnSetupDone() {
-		Console.WriteLine("done");
+		VulkanWindow0?.Show();
+		VulkanWindow1?.Show();
+	}
 
+	protected override void Setup() {
 		const string Title = "title goes here";
 
-		VulkanWindow = CreateWindow(GraphicsApi.Vulkan, $"vulkan. {Title}", 854, 480);
-		WindowRenderTarget vulkanWindowRenderTarget = new(this, VulkanWindow);
+		Console.WriteLine(Title);
 
-		OpenGLWindow = CreateWindow(GraphicsApi.OpenGL, $"opengl. {Title}", 854, 480);
-		WindowRenderTarget openglWindowRenderTarget = new(this, OpenGLWindow);
+		VulkanWindow0 = CreateWindow($"vulkan 0. {Title}", 854, 480);
+		VulkanWindow1 = CreateWindow($"vulkan 1. {Title}", 854, 480);
 
-		// SoftwareWindow = CreateWindow(GraphicsApi.Software, $"software. {Title}", 854, 480); // TODO opentk doesn't currently support making windows without OpenGL/Vulkan. look into this
-		// WindowRenderTarget softwareWindowRenderTarget = new(this, SoftwareWindow);
-
-		// EmptyWindow = CreateWindow(GraphicsApi.None, $"none. {Title}", 854, 480);
-		// WindowRenderTarget emptyWindowRenderTarget = new(this, EmptyWindow);
-
-		VulkanWindowRenderPass = new();
-		OpenGLWindowRenderPass = new();
-		// SoftwareWindowRenderPass = new();
-		// EmptyWindowRenderPass = new();
-
-		VulkanWindowRenderer = CreateRenderer(vulkanWindowRenderTarget, VulkanWindowRenderPass);
-		OpenGLWindowRenderer = CreateRenderer(openglWindowRenderTarget, OpenGLWindowRenderPass);
-		// SoftwareWindowRenderer = CreateRenderer(softwareWindowRenderTarget, SoftwareWindowRenderPass);
-		// EmptyWindowRenderer = CreateRenderer(emptyWindowRenderTarget, EmptyWindowRenderPass);
-
-		VulkanWindow.Show();
-		OpenGLWindow.Show();
-		// SoftwareWindow.Show();
-		// EmptyWindow.Show();
-
-		Console.WriteLine("show");
-
-		// RequestShutdown();
+		// VulkanWindow0RenderTarget = new(VulkanWindow0);
+		// VulkanWindow1RenderTarget = new(VulkanWindow1);
+		// ConsoleRenderTarget = new(false); // TODO test both
+		//
+		// TestRenderPass = new TestRenderPass();
+		//
+		// VulkanWindow0Renderer = CreateRenderer(VulkanWindow0RenderTarget, TestRenderPass);
+		// VulkanWindow1Renderer = CreateRenderer(VulkanWindow1RenderTarget, TestRenderPass);
+		// ConsoleRenderer = CreateRenderer(ConsoleRenderTarget, TestRenderPass);
 	}
 
-	protected override void Update() {
-		if (ReadonlyWindows.Count == 0) { RequestShutdown(); }
-	}
+	protected override void Update() { }
+
+	protected override void Cleanup() { }
 }
